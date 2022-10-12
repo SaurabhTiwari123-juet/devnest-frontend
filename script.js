ebd5753a74bbc214bb16a7bcd69b5e8c6538cf98
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2022-10-12 20:12:05 +0530

    new99

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 function handleClick() {
   alert("You have clicked a button");
 }
+
+document.getElementById("btnn").onclick = handleClick;
+
+//recommended way
+document.getElementById("btnn").addEventListener("click", handleClick);
+// js dom
